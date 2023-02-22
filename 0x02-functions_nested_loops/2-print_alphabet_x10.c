f5f35f378f6db6d3ab788d55473288aee583d154
sinemathonsi <sinemathonsi@gmail.com>
2023-02-22 08:04:28 -0800

    x10 alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function that pints 10x alphabet
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 1 ; i <= 10 ; i++)
+	{
+		for (j = 'a' ; j <= 'z' ; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
+}
