af1f8adcad35beed591c12ed9d4eb7424048179d
sinemathonsi <sinemathonsi@gmail.com>
2023-03-16 14:58:24 -0700

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -75,7 +75,7 @@ int main(int argc, char *argv[])
 	if (!result)
 		return (1);
 	for (i = 0; i <= len1 + len2; i++)
-		result[i]  0;
+		result[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
